155877734c3a431c4e01ba9e6dbe9d23f210532e
Maxim Zhiburt <zhiburt@gmail.com>
2019-10-20 10:29:35 +0300

    Update algorithm switch from maps of nodes to maps of indexes

--- graph/src/algorithm.rs
+++ graph/src/algorithm.rs
@@ -1,48 +1,58 @@
-use crate::node::{Node};
+use crate::node::{Graph};
 use std::collections::{BTreeMap, BTreeSet};
-use std::rc::Rc;
-use std::cell::RefCell;
 
-pub fn dijkstra<T: Eq + Ord>(source_node: Rc<RefCell<Node<T>>>, look: Rc<RefCell<Node<T>>>) {
-    let source = source_node.borrow();
-    if source.edges.is_none(){
-        return;
+pub fn dijkstra<T: Eq + Ord>(gh: &Graph<T>, source: usize, look: usize) -> Option<BTreeMap<usize,usize>> {
+    let src = gh.area[&source].borrow();
+    if src.edges.is_none(){
+        return None;
     }
 
+    let mut rev = BTreeMap::new();
     let mut checked = BTreeSet::new();
     let mut unchecked = BTreeSet::new();
     let mut dist = BTreeMap::new();
-    unchecked.insert(source_node.clone());
-    dist.insert(source_node.clone(), 0);
+    unchecked.insert(source);
+    dist.insert(source, 0);
 
     while !unchecked.is_empty() {
-        let (u, weight) = match dist.iter().map(|(u, w)| (u.clone(), *w)).filter(|(e, _)| !checked.contains(e)).min_by(|l, r| l.cmp(&r)) {
-            Some(d) => d,
+        let (u, weight) = match dist.iter().filter(|&(e, _)| !checked.contains(e)).min_by(|(_, lw), (_, rw)| lw.cmp(&rw)) {
+            Some(d) => (*d.0, *d.1),
             None => break,
         };
-        let uu = u.clone();
-        let uu = uu.borrow();
-        let edges = match uu.edges.as_ref() {
+        unchecked.remove(&u);
+
+        let u_node = gh.node_by_index(u).unwrap();
+        let u_node = u_node.borrow();
+        let edges = match u_node.edges.as_ref() {
             Some(e) => e,
-            None => break,
+            None => {
+                checked.insert(u);
+                continue;
+            }
         };
+
         for child in edges {
             
-            let check_node = &child.to;
+            let check_node = &child.to.borrow();
             let weight_from_source = child.weight + weight;
 
-            dist.entry(check_node.clone()).and_modify(|old_weight| {
+            dist.entry(check_node.index_in).and_modify(|old_weight| {
                 if weight_from_source < *old_weight {
                     *old_weight = weight_from_source;
                 }
             }).or_insert(weight_from_source);
 
-            unchecked.insert(check_node.clone());
+            unchecked.insert(check_node.index_in);
 
-            println!("{:?}", dist.values());
+            if !checked.contains(&check_node.index_in) {
+                rev.entry(check_node.index_in).and_modify(|old| {
+                    *old = u
+                }).or_insert(u);
+            }
         }
 
-        unchecked.remove(&u);
-        checked.insert(u.clone());
+        checked.insert(u);
     }
+
+    return Some(rev)
 }
