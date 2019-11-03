5946f781fc97278ec45e91e86d8b46bd824b85ad
Maxim Zhiburt <zhiburt@gmail.com>
2019-11-03 22:32:51 +0300

    Create H function for A* algorithm and use it

--- src/path_matrix.rs
+++ src/path_matrix.rs
@@ -2,8 +2,12 @@ use crate::pane::{MatrixPane};
 use graph::algorithm::{self};
 
 pub fn construct_path(mut matrix: MatrixPane, from: usize, look: usize, path_symbol: &str, checked_symbol: &str) -> MatrixPane {
-    let path = algorithm::dijkstra(matrix.graph(), from, look);
-        
+    let (dst_x, dst_y) = matrix.get_xy(look);
+    let path = algorithm::a_star(matrix.graph(), from, look, |n| {
+        let (x, y) = matrix.get_xy(n);
+        f64::abs(dst_x as f64 - x  as f64) as usize + f64::abs(dst_y as f64 - y  as f64) as usize
+    });
+
     for p in path.as_ref().unwrap() {
         matrix.get_node(*p.0).unwrap().borrow_mut().data = checked_symbol.to_owned();
     }
